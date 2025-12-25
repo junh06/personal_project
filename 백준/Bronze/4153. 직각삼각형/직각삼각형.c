67aa6ee39fc9853366469b8f7e4f73432b3d77a0
junh06 <pgh060426@gmail.com>
2025-12-25 23:26:06 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.c
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.c
@@ -6,62 +6,14 @@ int main(void)
     while(1)
     {
         scanf("%d %d %d", &a, &b, &c);
-        if(a == 0 && b == 0 && c == 0)
+        if(a == 0 && b == 0 && c == 0) break;
+        if((a*a + b*b == c*c) || (a*a + c*c == b*b) || (b*b + c*c == a*a))
         {
-            break;
+            printf("right\n");
         }
-        int max = (a > b) ? a : b;
-        if(max == a)
+        else
         {
-            max = (a > c) ? a : c;
-            if(max == a)
-            {
-                if(max * max == b*b + c*c)
-                {
-                    printf("right\n");
-                }
-                else
-                {
-                    printf("wrong\n");
-                }
-            }
-            else if(max == c)
-            {
-                if(max * max == a*a + b*b)
-                {
-                    printf("right\n");
-                }
-                else
-                {
-                    printf("wrong\n");
-                }
-            }
-        }
-        else if(max == b)
-        {
-            max = (b > c) ? b : c;
-            if(max == b)
-            {
-                if(max * max == a*a + c*c)
-                {
-                    printf("right\n");
-                }
-                else
-                {
-                    printf("wrong\n");
-                }
-            }
-            else if(max == c)
-            {
-                if(max * max == a*a + b*b)
-                {
-                    printf("right\n");
-                }
-                else
-                {
-                    printf("wrong\n");
-                }
-            }
+            printf("wrong\n");
         }
     }
     return 0;
